cdfc64ecab961118beb7cce6939ee0893406c05f
Nikson <niksonpoke@gmail.com>
2022-06-14 14:04:29 -0300

    realizando entrega

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Nikson"
+let sobrenome = "Gabriel"
+let anoNascimento = 2000
+let anoAtual = 2022
+let idade = anoAtual-anoNascimento
+let cidade = "Campina Grande"
+
+console.log(`Eu sou ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${cidade}.`)
